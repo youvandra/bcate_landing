7b8d3d9acdffcabf9b729a9415df3201c20274e0
Fauzan Pradana <fauzanpr.06@gmail.com>
2023-09-28 09:38:20 +0700

    fix(home): fixing large laptop responsive

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -6,45 +6,46 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className='mx-auto p-4 md:p-0'>
-            <div className={`w-full h-auto grid grid-cols-12 gap-8 md:px-4 md:items-center lg:bg-hero-bg lg:bg-cover lg:bg-bottom lg:px-4 lg:py-16 xl:py-24 xl:px-52`} >
-                <div className='col-span-12 md:col-span-6 mt-0 md:mt-10 flex flex-col space-y-6' >
-                    <h1 className='text-4xl md:text-6xl font-semibold'>BCate Project</h1>
-                    <p className='max-w-lg text-lg'>Welcome to Bcate 👋, where creativity meets blockchain, your gateway to a world of unique architectural designs!</p>
+            {/* <div className={`w-full h-auto grid grid-cols-12 gap-8 md:px-4 md:items-center lg:bg-hero-bg lg:bg-cover lg:bg-bottom lg:px-4 lg:py-16 xl:py-24`} > */}
+            <div className={`w-full h-auto md:items-center lg:bg-hero-bg lg:bg-cover lg:bg-bottom lg:py-16 xl:py-24`} >
+                <div className="w-full md:px-4 max-w-screen-xl m-auto">
+                    <div className='mb-4 mt-0 md:mt-10 lg:mb-0 flex flex-col space-y-6' >
+                        <h1 className='text-4xl md:text-6xl font-semibold'>BCate Project</h1>
+                        <p className='max-w-lg text-lg'>Welcome to Bcate 👋, where creativity meets blockchain, your gateway to a world of unique architectural designs!</p>
 
-                    <form className="max-w-md" >
-                        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
+                        <form className="max-w-md" >
+                            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
+                                </div>
+                                <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Search Mockups, Logos..." required />
+                                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2">Search</button>
                             </div>
-                            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Search Mockups, Logos..." required />
-                            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2">Search</button>
-                        </div>
-                    </form>
+                        </form>
 
-                    <div className="flex flex-row space-x-8 mt-12" >
-                        <div className="flex flex-col items-start" >
-                            <h1 className="text-2xl font-semibold" >1000+</h1>
-                            <p className="text-base text-gray-500">Blueprint</p>
+                        <div className="flex flex-row space-x-8 mt-12" >
+                            <div className="flex flex-col items-start" >
+                                <h1 className="text-2xl font-semibold" >1000+</h1>
+                                <p className="text-base text-gray-500">Blueprint</p>
+                            </div>
+                            <div className="flex flex-col items-start" >
+                                <h1 className="text-2xl font-semibold" >2354+</h1>
+                                <p className="text-base text-gray-500">Template</p>
+                            </div>
+                            <div className="flex flex-col items-start" >
+                                <h1 className="text-2xl font-semibold" >1000+</h1>
+                                <p className="text-base text-gray-500">And More..</p>
+                            </div>
                         </div>
-                        <div className="flex flex-col items-start" >
-                            <h1 className="text-2xl font-semibold" >2354+</h1>
-                            <p className="text-base text-gray-500">Template</p>
-                        </div>
-                        <div className="flex flex-col items-start" >
-                            <h1 className="text-2xl font-semibold" >1000+</h1>
-                            <p className="text-base text-gray-500">And More..</p>
-                        </div>
+
                     </div>
-
-                </div>
-                <div className='col-span-12 md:col-span-6'>
                     <Image src="/hero.svg" alt="" width={1000} height={1000} className="object-cove rounded-md lg:hidden" />
                 </div>
             </div>
-            <div className="my-12 md:px-4 lg:px-4 xl:px-52" >
+            <div className="my-12 md:px-4 max-w-screen-xl mx-auto" >
                 <p className="text-3xl font-semibold mb-12" >Why use Bcate?</p>
                 <div className="grid grid-cols-12 gap-4" >
                     <div className="col-span-6 md:col-span-3 flex flex-col space-y-2" >
@@ -77,7 +78,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="my-12 md:px-4 xl:px-52" >
+            <div className="my-12 md:px-4 max-w-screen-xl mx-auto" >
                 <p className="text-3xl font-semibold mb-2" >Lets explore</p>
                 <p className="mb-12" >We provide all kinds of furniture design and building architecture</p>
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
