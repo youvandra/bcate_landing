52eb283e6bd7d380b1a6eb82f74c3c799e67e28e
Youvandra Febrial <youvandrafebrial@Youvandras-Mac-mini.local>
2023-11-29 21:48:06 +0700

    delete card

--- src/modules/catalog/CatalogDetails/SuggestedProducts.tsx
+++ src/modules/catalog/CatalogDetails/SuggestedProducts.tsx
@@ -5,13 +5,6 @@ import Card from "@/components/Card"
 export default function SuggestedProducts() {
   return (
     <div className='mb-16 p-4'>
-        <hr />
-        <h2 className='text-sm p-4 bg-gray-700 text-white w-fit mb-8'>Mungkin Anda Suka</h2>
-        <div className='grid grid-cols-2 lg:grid-cols-5 gap-4'>
-            { [1,2,3,4,5,6,7,8].map(item => {
-                return <Card key={item} id={item.toString()} imageUrl="/kursi.png" name="Kursi Mahal" price={120000} />
-            }) }
-        </div>
     </div>
   )
 }
