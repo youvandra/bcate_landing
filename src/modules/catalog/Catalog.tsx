6d3ae05c906b5ee9be723f5770e4a5e53dd39434
Youvandra Febrial <youvandrafebrial@Youvandras-Mac-mini.local>
2023-11-29 21:46:06 +0700

    delete card

--- src/modules/catalog/Catalog.tsx
+++ src/modules/catalog/Catalog.tsx
@@ -5,9 +5,6 @@ import Card from "@/components/Card";
 const Catalog = () => {
     return (
         <div className="max-w-screen-xl p-8">
-            <div className="md:grid md:grid-cols-5 gap-4">
-                {[1, 2, 3, 4, 5].map(num => <Card key={num} id={num.toString()} imageUrl="/kursi.png" name="Kursi Mahal" price={1250000} />)}
-            </div>
         </div>
     );
 }
