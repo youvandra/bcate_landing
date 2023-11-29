bd2eee53b3c40e4c4df91a057edabc1403b263f8
galihpermana292 <galihcbn123@gmail.com>
2023-11-29 16:27:21 +0700

    adding wagmi

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,46 +1,88 @@
-"use client"
+'use client';
 
-import Card from "@/components/Card";
-import { randomId } from "@/utils/generator";
+import Card from '@/components/Card';
+import { randomId } from '@/utils/generator';
+import useWrite from '@/utils/useWrite';
+import { useMetaMask } from 'metamask-react';
+import { UseContractWriteConfig, usePrepareContractWrite } from 'wagmi';
+
+import abi from '@/utils/ABI.json';
 
 export default function CatalogPage() {
-    return (
-  
-        <div className="max-w-screen-xl mx-auto grid grid-cols-5 gap-8 p-4">
-            <div className="col-span-5" >
-                <form>
-                    <div className="flex">
-                        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search Catalog</label>
-                        <div className="relative w-full">
-                            <input type="search" id="search-dropdown"
-                                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                                placeholder="Search" required />
-                            <button type="submit"
-                                className="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-gray-800 hover:bg-gray-900 rounded-r-lg border focus:ring-4 focus:outline-none focus:ring-blue-300"><svg
-                                    className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+	const { account } = useMetaMask();
 
-            {/* <div className="hidden lg:block lg:col-span-1" >
+	const { config } = usePrepareContractWrite({
+		address: '0xD365EA07e1b903358FE352AE5C80A4055d5A80C2' as `0x${string}`,
+		abi: abi,
+		functionName: 'mintTicket',
+		// args: [
+		// 	address, // address wallet
+		// 	param?.id, // id
+		// ],
+	}) as unknown as {
+		config: UseContractWriteConfig;
+	};
+
+	const {
+		data: { isLoading, error, isSuccess },
+		write,
+	} = useWrite(config);
+	return (
+		<div className="max-w-screen-xl mx-auto grid grid-cols-5 gap-8 p-4">
+			<div className="col-span-5">
+				<form>
+					<div className="flex">
+						<label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+							Search Catalog
+						</label>
+						<div className="relative w-full">
+							<input
+								type="search"
+								id="search-dropdown"
+								className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+								placeholder="Search"
+								required
+							/>
+							<button
+								type="submit"
+								className="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-gray-800 hover:bg-gray-900 rounded-r-lg border focus:ring-4 focus:outline-none focus:ring-blue-300">
+								<svg
+									className="w-4 h-4"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 20 20">
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+									/>
+								</svg>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+
+			{/* <div className="hidden lg:block lg:col-span-1" >
                 <CatalogSidebar />
             </div> */}
 
-            <div className="col-span-5 lg:col-span-5" >
-                <div className="grid grid-cols-2 lg:grid-cols-4 gap-8" >
-                    {
-                        randomId(10).map(num => (
-                            <Card key={num} id={num} imageUrl="/kursi.png" name="Kursi Mahal" price={100} />
-                        ))
-                    }
-                </div>
-            </div>
-
-        </div>
-    );
+			<div className="col-span-5 lg:col-span-5">
+				<div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+					{randomId(10).map((num) => (
+						<Card
+							key={num}
+							id={num}
+							imageUrl="/kursi.png"
+							name="Kursi Mahal"
+							price={100}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,82 +1,83 @@
-"use client";
+'use client';
 
-import { CHAIN_ID, CHAIN_PARAMS } from "@/utils/constant";
-import { truncate } from "@/utils/generator";
+import { CHAIN_ID, CHAIN_PARAMS } from '@/utils/constant';
+import { truncate } from '@/utils/generator';
 
-import { showNotification } from "@mantine/notifications";
-import { useMetaMask } from "metamask-react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { showNotification } from '@mantine/notifications';
+import { useMetaMask } from 'metamask-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export const WalletButton = () => {
-  const router = useRouter();
-  const { status, connect, addChain, chainId, switchChain, account } = useMetaMask();
-  const [account_wallet, setWallet] = useState('');
+	const router = useRouter();
+	const { status, connect, addChain, chainId, switchChain, account } =
+		useMetaMask();
+	const [account_wallet, setWallet] = useState('');
 
-  const handleConnect = async () => {
+	const handleConnect = async () => {
+		console.log('hello');
 
-    console.log('hello')
+		try {
+			await connect();
+		} catch (error) {
+			showNotification({
+				message: 'Failed to connect with MetaMask',
+				color: 'red',
+			});
+		}
+	};
 
-    try {
-      await connect();
-    } catch (error) {
-      showNotification({
-        message: "Failed to connect with MetaMask",
-        color: "red",
-      });
-    }
+	const handleNetworkChange = async () => {
+		try {
+			await switchChain(CHAIN_ID);
+			router.refresh();
+		} catch (error: any) {
+			if (error.code === 4902) {
+				await addChain(CHAIN_PARAMS);
+				await switchChain(CHAIN_ID);
+				router.refresh();
+			} else {
+				showNotification({
+					message: 'Failed to change network',
+					color: 'red',
+				});
+			}
+		}
+	};
 
+	useEffect(() => {
+		if (status === 'notConnected') {
+			showNotification({
+				message: 'MetaMask is not connected yet!',
+				color: 'red',
+			});
+		} else if (status === 'unavailable') {
+			showNotification({
+				message: 'No wallet detected!',
+				color: 'red',
+			});
+		} else if (status === 'connected') {
+			setWallet(account.toLowerCase());
+		}
+	}, [status, account]);
 
-  };
+	useEffect(() => {
+		if (status === 'connected' && chainId !== CHAIN_ID) {
+			handleNetworkChange();
+			showNotification({ message: 'Wrong network!', color: 'red' });
+		}
+	}, [status, chainId]);
 
-  const handleNetworkChange = async () => {
-    try {
-      await switchChain(CHAIN_ID);
-      router.refresh();
-    } catch (error: any) {
-      if (error.code === 4902) {
-        await addChain(CHAIN_PARAMS);
-        await switchChain(CHAIN_ID);
-        router.refresh();
-      } else {
-        showNotification({
-          message: "Failed to change network",
-          color: "red",
-        });
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (status === "notConnected") {
-      showNotification({
-        message: "MetaMask is not connected yet!",
-        color: "red",
-      });
-    } else if (status === "unavailable") {
-      showNotification({
-        message: "No wallet detected!",
-        color: "red",
-      });
-    } else if (status === "connected") {
-      setWallet(account.toLowerCase());
-    }
-  }, [status, account]);
-
-  useEffect(() => {
-    if (status === "connected" && chainId !== CHAIN_ID) {
-      handleNetworkChange();
-      showNotification({ message: "Wrong network!", color: "red" });
-    }
-  }, [status, chainId]);
-
-  console.log(account_wallet);
-  return (
-    <>
-      <button onClick={() => handleConnect()} type="button" className="hidden md:inline-block text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">
-        {status === 'initializing' || status === 'notConnected' ? 'Connect Wallet' : truncate(account_wallet, 4, 4, 11)}
-      </button>
-
-    </>
-  );
-}
+	return (
+		<>
+			<button
+				onClick={() => handleConnect()}
+				type="button"
+				className="hidden md:inline-block text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">
+				{status === 'initializing' || status === 'notConnected'
+					? 'Connect Wallet'
+					: truncate(account_wallet, 4, 4, 11)}
+			</button>
+		</>
+	);
+};
